4ddf3ae2168548c4b8123748946f823c1c8322b2
dkrzoska <d.krzoska@gmail.com>
2020-08-16 17:23:12 +0200

    add remove info in dashboard

---
+++ development/js/app.js
@@ -0,0 +1,7 @@
+//close info in dash board
+const closeInfo = document.querySelectorAll(".widget");
+closeInfo.forEach ( el => {
+    el.lastElementChild.addEventListener("click", ev => {
+        el.parentElement.removeChild(el);
+    });
+});
